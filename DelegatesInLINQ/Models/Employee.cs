e13b47dfcb5d11395e12f053d5e59eb7c8d2ba90
Petro <kolosovp94@gmail.com>
2020-12-25 16:26:54 +0100

    initial commit

---
+++ DelegatesInLINQ/Models/Employee.cs
@@ -0,0 +1,22 @@
+﻿namespace DelegatesInLINQ.Models
+{
+    public class Employee
+    {
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public int Age { get; set; }
+        public double Salary { get; set; }
+
+        public Employee(string firstname, string lastname, int age, double salary)
+        {
+            Firstname = firstname;
+            Lastname = lastname;
+            Age = age;
+            Salary = salary;
+        }
+
+        public Employee()
+        {
+        }
+    }
+}
---
+++ DelegatesInLINQ/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+using DelegatesInLINQ.Models;
+
+namespace DelegatesInLINQ
+{
+    public static class Program
+    {
+        private static readonly Func<Employee, int, bool> AgeIsGreaterFunc = (employee, i)
+            => employee.Age > i;
+
+        private static readonly Func<Employee, double, bool> SalaryIsGreaterFunc = (employee, i)
+            => employee.Salary > i;
+
+        private static readonly Predicate<Employee> AgeIsGreaterPredicate = employee => employee.Age > 20;
+
+        private static void Main()
+        {
+            var employees = new[]
+            {
+                new Employee("Ivan", "Ivanov", 20, 150.2),
+                new Employee("Andrey", "Andreev", 30, 250.2),
+                new Employee("Winston", "Cherchill", 40, 320.2),
+                new Employee("Arkadiy", "Kulakosvkiy", 50, 420.2),
+            };
+
+            var selectAgeFunc = employees.Where(e => AgeIsGreaterFunc(e, 20)).ToList();
+            selectAgeFunc.ForEach(e => Console.WriteLine($"{e.Firstname}, {e.Age}"));
+
+            var selectSalaryFunc = employees.Where(e => SalaryIsGreaterFunc(e, 200)).ToList();
+            selectSalaryFunc.ForEach(e => Console.WriteLine($"{e.Firstname}, {e.Salary}"));
+
+            var selectAgePredicate = employees.Where(e => AgeIsGreaterPredicate(e)).ToList();
+            selectAgePredicate.ForEach(e => Console.WriteLine($"{e.Firstname}, {e.Age}"));
+        }
+    }
+}
